3370874867e1c7c031d7783e050a8b05c2b8bf40
Anthony Littlejohn <ant05man@yahoo.com>
2023-07-27 14:31:56 -0400

    centered header

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-$(function () {
+var timeDisplayEl = $('#currentDay');
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -20,4 +20,10 @@ $(function () {
     // attribute of each time-block be used to do this?
     //
     // TODO: Add code to display the current date in the header of the page.
-  });
+
+      function displayTime() {
+      var rightNow = dayjs().format('MMM D,YYYY');
+      timeDisplayEl.text(rightNow);
+      }
+
+      displayTime();
