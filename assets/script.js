9efac4151740ca25d75ab93ceab4242c141b6c00
Anthony Littlejohn <ant05man@yahoo.com>
2023-07-29 21:05:18 -0400

    added link so I am able to use moment function

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,22 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-var todayDate = moment().format('dddd, MMM do YYYY');
+var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
-var timeDisplayEl = $('#currentDay');
+
+//whatever written inside the $(document).ready will run once the page DOM is ready for JS code
+$(document).ready(function() {
+
+
     // TODO: Add a listener for click events on the save button. This code should
+    $(".saveBtn").on("click", function() {
+
+      var text = $(this).siblings(".description").val();
+      
+      var time = $(this).parent().attr("id");
+
+      localStorage.setItem(time,text);
+    })
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
     // function? How can DOM traversal be used to get the "hour-x" id of the
@@ -23,37 +35,43 @@ var timeDisplayEl = $('#currentDay');
     //
     // TODO: Add code to display the current date in the header of the page.
 
-      function displayTime() {
-      var rightNow = dayjs().format('MMM D,YYYY');
-      timeDisplayEl.text(rightNow);
-      }
-
-      displayTime();
 
       //Get current number of hours
       function timeTracker() {
         // moment() - helps manipulate, displaying date/time
         // hour() - communicates w/ CPU clock returns current hour as value from 0-23
-        var currentTime = moment().hour();
-      }
+        var currentTime = moment().hours();
+        console.log("hours is: ", currentTime);
 // This will loop over time-blocks 
 $(".time-block").each(function() {
-  var blockTime = parseInt($(this).attr("id").split("hour")[9]);
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
 
   //Checks the Time and add the appropriate classes and applies background colors
-  if (blockTIme < currentTIme) {
-    $ (this).removeClass("future");
-    $ (this).removeClass("present");
-    $ (this).addClass("past");
+  if (blockTime < currentTime) {
+    $(this).removeClass("future");
+    $(this).removeClass("present");
+    $(this).addClass("past");
   }
   else if (blockTime === currentTime) {
-    $ (this).removeClass("future");
-    $ (this).addClass("present");
-    $ (this).removeClass("past");
+    $(this).removeClass("past");
+    $(this).removeClass("future");
+    $(this).removeClass("past");
   }
   else {
-    $ (this).addClass("future");
-    $ (this).removeClass("present");
-    $ (this).removeClass("past");
+    $(this).removeClass("present");
+    $(this).removeClass("pasat");
+    $(this).addClass("future");
   }
-});
+
+})
+
+}
+
+$("#hour9 .description").val(localStorage.getItem("hour9"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+
+
+timeTracker();
+
+})
+
